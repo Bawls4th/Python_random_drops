fa503ec77a2b27c349cb59b422a3ceffa1fe60e2
Bawls4th <juliusrodeman@gmail.com>
2024-09-04 16:02:18 +0800

    Add files via upload

---
+++ Penis_LengthcyKa.py
@@ -0,0 +1,23 @@
+size = int(input("Enter your length stereotype: "))
+pp_Length = ('8'+'='*size+'D')
+
+if size == 0 or size == 1 or size == 2:
+    print(pp_Length)
+    print("Trans Skill issue.. Fagoot..")
+elif size == 3 or size == 4:
+    print(pp_Length)
+    print("smol wee wee, ola Chinese...")
+elif size == 5 or size == 6:
+    print(pp_Length)
+    print("avg size, Pleasing enough...")
+elif size == 7 or size == 8:
+    print(pp_Length)
+    print("PornStar Ready, AIDS PASSER CONFIRM...")
+elif size == 9 or size == 10:
+    print(pp_Length)
+    print("BBC gifted, I NATE Higgers...")
+else:
+   print(pp_Length)
+   print("Tentacle shits...")
+
+
